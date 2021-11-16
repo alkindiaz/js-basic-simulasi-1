3dbfec9ae7983bcb6e86b1d7c4069e92ffb8559d
alkindiaz <alkindi.ibn.az@gmail.com>
2021-11-16 08:32:42 +0700

    no 3 done

--- 3.js
+++ 3.js
@@ -16,3 +16,21 @@
  *   - Tidak diperbolehkan menggunakan built-in function:
  *     .map .filter .reduce .split .join .indexOf .findIndex .substring
  */
+
+ function getProvider(email) {
+    let temp = ''
+    let flag = false;
+    for(let i = 0; i < email.length; i++) {
+        if(email[i-1] === "@") {
+            flag = true
+        } else if (email[i] === ".") flag = false
+        if(flag) {
+            temp += email[i]
+        }
+    }
+    return `your email provider is ${temp}`
+}
+
+console.log(getProvider('alkindi@yahoo.com'))
+console.log(getProvider('lisa@gmail.com'))
+console.log(getProvider('naruto@konoha.com'))

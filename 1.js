4f6f57d7eabc325ff7d193a5b9a09a5b9c3012e2
alkindi az <37734368+alkindiaz@users.noreply.github.com>
2021-11-16 08:26:18 +0700

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,16 @@
+/**
+ * DISNEY ISLAND
+ * =============
+ * 
+ * Buatlah pseudocode untuk kasus berikut:
+ * 
+ * Sebuah wahana bermain 'Disney Island' akan memberikan tarif sesuai dengan tinggi dan umur anak
+ * dengan ketentuan sebagai berikut:
+ *   - Umur 1 tahun ke bawah: Tampilkan 'Dilarang masuk'
+ *   - Umur 2-3 tahun: Rp 30.000. Jika tinggi anak umur 2-3 tahun lebih dari 70cm maka tarif akan bertambah Rp. 10.000.
+ *   - Umur 4-7 tahun: Rp 40.000. Jika tinggi anak umur 4-7 tahun lebih dari 120cm maka tarif akan bertambah Rp. 15.000.
+ *   - Umur 8-10 tahun: Rp 50.000. Jika tinggi anak umur 8-10 tahun lebih dari 150cm maka tarif akan bertambah Rp. 20.000.
+ *   - Umur diatas 10 tahun : Rp 80.000.
+ * 
+ * Tampilkan tarif harga sesuai umur dan tinggi seorang anak!
+ */
---
+++ 3.js
@@ -0,0 +1,18 @@
+/**
+ * WHAT IS MY EMAIL PROVIDER?
+ * ==========================
+ *
+ * Email merupakan sebuah cara untuk kita berinteraksi antar satu dengan yang lainnya secara elektronik,
+ * Banyak sekali provider yang menyediakan layanan email ini.
+ *
+ * Buatlah sebuah proses yang akan mengeluarkan output provider email yang digunakan oleh user.
+ *
+ * Contoh:
+ *   - Input  : icha@gmail.com
+ *   - Output : Your email provider is gmail
+ *     (tidak menggunakan .com di belakang)
+ * 
+ * RULES:
+ *   - Tidak diperbolehkan menggunakan built-in function:
+ *     .map .filter .reduce .split .join .indexOf .findIndex .substring
+ */
